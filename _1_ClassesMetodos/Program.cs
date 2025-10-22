0bd57e318c8ea036a52b78e08ed699f237b7ee2a
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-10-22 08:24:36 -0300

    adicionando classes e Instanciando

--- _1_ClassesMetodos/Program.cs
+++ _1_ClassesMetodos/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Classes e Métodos ##\n");
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Ana";
+p1.Idade = 28;
+p1.Sexo = "Feminino";
+
+Console.WriteLine($"Nome: {p1.Nome}, {p1.Idade}, {p1.Sexo}");
+
+Pessoa p2 = new();
+p2.Nome = "Gabrielle";
+p2.Idade = 25;
+p2.Sexo = "Feminino";
+
+Console.WriteLine($"Nome: {p2.Nome}, {p2.Idade}, {p2.Sexo}");
+
+Console.ReadLine();
+
+public class Pessoa
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+
+    // A nomenclatura usada para declarar campos é a Pascal Case.
+    // Assim o correto seria usar nomes com (iniciais maiúsculas): Nome, Idade e Sexo.
+}
