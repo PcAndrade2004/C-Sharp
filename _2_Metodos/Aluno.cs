6692bfbf1a18cf45c94176ab7f4d7bd65f440654
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-10-22 11:18:19 -0300

    Simplificando o método utilizando o =>

---
+++ _2_Metodos/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Metodos
+{
+    public class Aluno
+    {
+        public string Nome;
+        public int Idade;
+        public string Sexo;
+        public string Aprovado;
+    }
+}
--- _2_Metodos/Program.cs
+++ _2_Metodos/Program.cs
@@ -1,12 +1,35 @@
-﻿Console.WriteLine("## Métodos ##\n");
+﻿using _2_Metodos;
 
-MinhaClasse minhaClasse = new(); // Criando uma instância da classe MinhaClasse
-minhaClasse.Saudacao();// Chamando o método Saudacao da classe MinhaClasse
-minhaClasse.ExibirDataAtual(); 
+Console.WriteLine("## Métodos ##\n");
+
+//Console.WriteLine("Métodos sem Parâmetros");
+//MinhaClasse minhaClasse = new(); // Criando uma instância da classe MinhaClasse
+//minhaClasse.Saudacao();// Chamando o método Saudacao da classe MinhaClasse
+//minhaClasse.ExibirDataAtual();
+
+//Console.WriteLine("\nMétodos com Parâmetros");
+//MetodoParametro metodoParametro = new();
+//metodoParametro.ExibirInformacoes("Paulo Cesar", DateTime.Now.ToShortDateString());
+
+
+Aluno aluno = new Aluno();
+
+Console.WriteLine("Nome : ");
+aluno.Nome = Console.ReadLine();
+Console.WriteLine("Idade : ");
+aluno.Idade = int.Parse(Console.ReadLine());
+Console.WriteLine("Sexo : ");
+aluno.Sexo = Console.ReadLine();
+Console.WriteLine("Aprovado (Sim/Não) : ");
+aluno.Aprovado = Console.ReadLine();
+
+Curso curso = new Curso();  
+curso.Resultado(aluno);
 
 
 Console.ReadKey();
 
+#region Métodos com Sem Parâmetros
 public class MinhaClasse
 {
     public void Saudacao()
@@ -17,3 +40,21 @@ public class MinhaClasse
 
     public void ExibirDataAtual() => Console.WriteLine($"Data atual: {DateTime.Now.ToShortDateString()}");
 }
+#endregion
+
+#region Métodos com Parâmetros
+// Definindo os métodos com parâmetros.
+// Parâmetros permitem que você passe informações para dentro do método quando ele é chamado.
+
+public class MetodoParametro()
+{
+    public void ExibirInformacoes(string nome, string data)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(data);
+    }
+    
+}
+
+#endregion
+
