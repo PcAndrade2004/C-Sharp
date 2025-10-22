9ba915608df12f2f714865f7836339c0ab12fd9f
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-10-22 11:37:50 -0300

    Construtores

---
+++ _3_Construtores/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Construtores
+{
+    public class Aluno
+    {
+        public string? Nome;
+        public int Idade;
+        public string? Materia;
+        public string? Aprovado;
+
+        // Construtor com parâmetros
+        public Aluno(string nome, int idade, string materia, string aprovado)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Materia = materia;
+            this.Aprovado = aprovado;
+        }
+        public void exibir()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Idade: " + Idade);
+            Console.WriteLine("Matéria: " + Materia);
+            Console.WriteLine("Aprovado: " + Aprovado);
+        }
+    }
+}
