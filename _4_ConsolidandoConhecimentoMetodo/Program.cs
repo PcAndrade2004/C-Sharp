a56a0dba1c8287f9c4f96173254be2e9c5697cea
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-10-23 20:29:32 -0300

    Consolidando Conhecimento, sobrecargas e métodos

---
+++ _4_ConsolidandoConhecimentoMetodo/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("## Consolidando Conhecimento Métodos ##\n");
+
+Cadastro cadastro = new Cadastro(); 
+var cliente = cadastro.Registrar(); 
+cadastro.Exibir(cliente);
+
+cliente = cadastro.Registrar(cliente);
+cadastro.Exibir(cliente);
+
+Console.ReadKey();  
+
+public class Cliente
+{
+    public string? Nome;
+    public int Idade;
+    public decimal Renda;
+
+    public Cliente(string? nome, int idade, decimal renda)
+    {
+        Nome = nome;
+        Idade = idade;
+        Renda = renda;
+    }
+    public Cliente()// ctor TAB + TAB cria o construtor Vazio
+    { }
+}
+
+public class  Cadastro
+{
+    public Cliente Registrar()
+    {
+        Cliente cliente = new Cliente("Paulo0", 23, 3000);
+        return cliente;
+    }
+    public Cliente Registrar(Cliente cliente)
+    {
+        cliente.Renda = 5000;
+        return cliente;
+    }
+
+    public void Exibir(Cliente cliente)
+    {
+        Console.WriteLine($"Nome: {cliente.Nome}\nIdade: {cliente.Idade}\nRenda: {cliente.Renda.ToString("C")}");
+    }
+
+
+}
