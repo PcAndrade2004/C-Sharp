c1f0373d078f4fcb7a4317bd64a7f3e3d2e7c997
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-10-28 09:16:03 -0300

    Adicionando conhecimento

---
+++ _9_ValidandoConhecimento4/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("## Validando Conhecimento 4 ##\n");  
+
+Console.ReadKey();
+
+
+public class Produto
+{
+    private string nome;
+    private decimal preco;
+    private int quantidade;
+
+    public string Nome
+    {
+        get => nome;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine("Nome não pode ser vazio.");  
+            }else
+            {
+                nome = value;
+            }
+        }
+    }
+
+    public void Preco
+    {
+        get => preco;
+        set
+        {
+            if ()
+            {
+
+            }
+        }
+    }
+}
