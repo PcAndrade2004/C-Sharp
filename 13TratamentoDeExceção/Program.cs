699a65b3552ecfaa215b332e78a06b395814a0d8
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-10-31 09:14:25 -0300

    Estudo sobre bloco Try-Catch e Finally

--- 13TratamentoDeExceção/Program.cs
+++ 13TratamentoDeExceção/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Tratamento de Exceções Try-Catch ##\n");
+
+try
+{
+    Console.WriteLine("\nInforme o valor de x ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("\nInforme o valor de y ");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    int z = x / y;
+    Console.WriteLine($"\nO valor de z é: {z}");
+
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine($"\nErro: <<< 10{e.Message} >>>");  
+    Console.WriteLine($"\nDetalhes {e?.StackTrace?.ToString()}"); 
+    
+}
+
+
+Console.ReadKey();  
