35a5bd4ed07af94274cc89dd1dbd1002de680db2
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 19:08:05 -0300

    Membros Estaticos

---
+++ Fundamentos/_7_MembrosEstaticos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Membros Estáticos ##\n");
+
+var cliente1 = Cliente.Nome = "Ana";
+
+Console.ReadKey();
+
+public class Cliente
+{
+    public static string? Nome; // São campos de instância
+    public static int Idade;
+
+    public Cliente(string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
+
+}
