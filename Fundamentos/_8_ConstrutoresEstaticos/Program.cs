66ad9a5f573a7da18378369202ad778ad949336b
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 19:09:07 -0300

    Construtores Estaticos

---
+++ Fundamentos/_8_ConstrutoresEstaticos/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("## Construtores Estáticos ##\n");
+
+Pessoa p1 = new Pessoa(25, "João");
+
+Console.WriteLine($"Nome: {p1.Nome}, Idade: {p1.Idade}");
+Console.WriteLine($"Idade Mínima: {Pessoa.IdadeMinima}");
+
+
+
+Console.ReadKey();
