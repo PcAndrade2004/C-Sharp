a19dd08a07c3bf881d625a9040a974eb4a45db24
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 20:46:26 -0300

    Laço for

---
+++ Array-ArrayList-List/_2_ArrayLacoFor/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("## Laço For em Arrays ##\n");
+
+string[] nomes = { "Paulo", "Gabrielle", "Arthur", "Carlos", "Diomeneis", "Claudia" };
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Nome do índice {i} : {nomes[i]}");
+}
+
+Console.ReadKey();
