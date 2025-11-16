5e24470f25d6ae0df4318cdd7f0b9452d6be8538
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 20:48:02 -0300

    Acessando Laço foreach

---
+++ Array-ArrayList-List/_3_ArrayLacoForeach/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Laço Foreach em Arrays ##\n");
+
+#region Laço foreach com nomes
+string[] nomes = { "Paulo", "Gabrielle", "Arthur", "Carlos", "Diomeneis", "Claudia" };
+
+foreach (var nome in nomes)
+{
+    Console.WriteLine($"Nome: {nome}");
+}
+#endregion
+
+Console.WriteLine("--------------------------------------");
+
+#region Laço foreach com números
+int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+foreach (var numero in numeros)
+{
+    Console.WriteLine($"Número: {numero}");
+}
+#endregion
+
+Console.ReadKey();
