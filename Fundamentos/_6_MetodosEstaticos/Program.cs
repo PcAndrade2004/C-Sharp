72b536662064aa26763923f60546701114c2c75f
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 19:07:05 -0300

    Métodos Estaticos

---
+++ Fundamentos/_6_MetodosEstaticos/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Métodos Estaticos ##\n");
+
+var somar = Calculadora.Somar(10, 5);
+var subtrair = Calculadora.Subtrair(10, 5);
+
+Console.WriteLine(somar);
+Console.WriteLine(subtrair);
+
+Console.ReadKey();
+
+public class Calculadora
+{
+    // Método estático
+    public static int Somar(int a, int b)
+    {
+        return a + b;
+    }
+    // Método de instância
+    public static int Subtrair(int a, int b)
+    {
+        return a - b;
+    }
+
+}
