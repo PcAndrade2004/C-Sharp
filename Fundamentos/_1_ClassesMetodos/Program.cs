715367b621b17b4ae90c9b35be7ad3bea60ecb12
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 18:58:25 -0300

    adicionando

---
+++ Fundamentos/_1_ClassesMetodos/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("## Classes e Métodos ##\n");
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Ana";
+p1.Idade = 28;
+p1.Sexo = "Feminino";
+
+Console.WriteLine($"Nome: {p1.Nome}, {p1.Idade}, {p1.Sexo}");
+
+Pessoa p2 = new();
+p2.Nome = "Gabrielle";
+p2.Idade = 25;
+p2.Sexo = "Feminino";
+
+Console.WriteLine($"Nome: {p2.Nome}, {p2.Idade}, {p2.Sexo}");
+
+Console.ReadKey();
+
+public class Pessoa
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+
+    // A nomenclatura usada para declarar campos é a Pascal Case.
+    // Assim o correto seria usar nomes com (iniciais maiúsculas): Nome, Idade e Sexo.
+}
