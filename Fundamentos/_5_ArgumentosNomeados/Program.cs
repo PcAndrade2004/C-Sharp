923f76fce5eecd1fa4f5dc0f71f02a6d211f146a
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 20:37:05 -0300

    Argumentos Nomeados

--- Fundamentos/_5_ArgumentosNomeados/Program.cs
+++ Fundamentos/_5_ArgumentosNomeados/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Argumentos Nomeados ##\n");
+
+
+Console.ReadKey();
+
+public class Email
+{
+    public void Enviar(string destino, string titulo, string assunto)
+
+    {
+        Console.WriteLine($"Enviando email para {destino}, com o título");
+    }
+}
+
