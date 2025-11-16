8fa7e757f02c4425d40041161f4da918790e3d68
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 19:02:03 -0300

    Métodos em C#

---
+++ Fundamentos/_2_Metodos/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Metodos
+{
+    public class Aluno
+    {
+            public string Nome;
+            public int Idade;
+            public string Sexo;
+            public string Aprovado;
+        
+    }
+}
