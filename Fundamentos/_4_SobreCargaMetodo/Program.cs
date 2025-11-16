48b67b6fbbdf4feb05ffb86bcdc30649f5b6feeb
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 19:04:59 -0300

    Sobrecarga de Métodos

---
+++ Fundamentos/_4_SobreCargaMetodo/Program.cs
@@ -0,0 +1,19 @@
+﻿using _4_SobrecargaMetodo;
+
+Console.WriteLine("## Sobrecarga de Métodos ##\n");
+
+Calculadora calculadora = new();
+calculadora.Somar(5, 10);                // Chama o método que soma dois inteiros
+calculadora.Somar(5.5, 10.3);            // Chama o método que soma dois doubles
+calculadora.Somar(1, 2, 3);              // Chama o método que soma três inteiros
+calculadora.Somar("Olá, ", "Mundo!");   // Chama o método que concatena duas strings
+
+Email email = new();
+email.Enviar("empresa.com.br");
+email.Enviar("usuario", "empresa.com.br");
+email.Enviar("empresa.com.br", 2000);
+
+
+
+Console.ReadKey();
+
