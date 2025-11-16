fafa625e120f55d3daa8db1122079c5c6fb50cf2
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-16 19:09:53 -0300

    Encapsulamento

---
+++ Fundamentos/_9_Encapsulamento/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("## Propriedades ##\n");
+
+Console.WriteLine("Informe o ano do Carro: ");
+var ano = Convert.ToInt32(Console.ReadLine());
+
+Carro carro = new Carro();
+carro.Ano = ano;
+
+Console.WriteLine($"O ano do carro é: {carro.Ano}");
+
+Console.ReadKey();
+
+public class Carro
+{
+    private int ano;
+    public int Ano
+    {
+        get => ano;
+        set
+        {
+            if (value < 2000)
+            {
+                ano = 2000;
+            }
+            else if (value > 2022)
+            {
+                ano = 2022;
+            }
+            else
+            {
+                ano = value;
+            }
+        }
+    }
+
+
+}
