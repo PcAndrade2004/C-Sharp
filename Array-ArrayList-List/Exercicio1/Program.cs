13cb1232103144cdd0955773753c6ebfcef333fc
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-22 18:17:27 -0300

    Exercicio Adicionando elemento

---
+++ Array-ArrayList-List/Exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("## Criar e adicionar elemento ##\n");
+
+List<string> nomes = new List<string>();
+
+
+nomes.Add("Paulo Cesar");
+nomes.Add("Gabrielle Oliveira");
+nomes.Add("Arthur Araujo");
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine(nome);
+}
+
+
+Console.ReadKey();
