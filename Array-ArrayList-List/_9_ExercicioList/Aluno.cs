9c59e6c3c1c0081807fa83818cddf29c0b3d516e
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-22 18:11:42 -0300

    Exercicios Aluno

---
+++ Array-ArrayList-List/_9_ExercicioList/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Exercicio
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public Double Nota { get; set; }
+
+        public Aluno(string nome, double nota)
+        {
+            this.Nome = nome;
+            this.Nota = nota;
+        }
+
+        public Aluno()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Nota}";
+        }
+    }
+}
---
+++ Array-ArrayList-List/_9_ExercicioList/Program.cs
@@ -0,0 +1,58 @@
+﻿using _9_Exercicio;
+using _9_ExercicioList;
+
+Console.WriteLine("## Exercicio ##\n");
+
+List<Aluno> alunos = DadosEntrega.getAlunos();
+
+ExibirAlunos(alunos);
+
+#region Adicionando novos alunos
+Aluno bia = new Aluno() { Nome = "Bia", Nota = 7.75 };
+Aluno mario = new Aluno() { Nome = "Mario", Nota = 8.95 };
+alunos.Add(bia);
+alunos.Add(mario);
+#endregion
+
+ExibirAlunos(alunos); 
+
+#region Localizando o aluno 
+Console.WriteLine("##\nBusca aluno e removendo ##");
+var localizarAluno = alunos.Find(n=> n.Equals("Amanda"));
+alunos.Remove(localizarAluno);
+ExibirAlunos(alunos);
+#endregion
+
+#region Ordenando lista pelo nome
+Console.WriteLine("## Ordenando Lista ##");
+var listaOrdenada = alunos.OrderBy(a => a.Nome).ToList();
+ExibirAlunos(alunos);
+#endregion
+
+#region Notas maior que 8
+var notaMaior = alunos.FindAll(a => a.Nota >= 8);
+foreach(var teste in alunos)
+{
+    Console.WriteLine(teste.Nome);
+}
+#endregion
+
+
+
+Console.ReadKey();
+
+static void ExibirAlunos(List<Aluno> alunos)
+{
+    //Criei um método para que ficasse mais facil de exibir os alunso. Porque irei precisar de novo.
+    Console.WriteLine("\nNomes na lista principal: ");
+    var somaNotas = 0.0;
+    foreach (var item in alunos)
+    {
+        Console.WriteLine($"{item.Nome}\t{item.Nota}");
+        somaNotas = +item.Nota;
+    }
+
+    var mediaNotas = somaNotas / alunos.Count;
+    Console.WriteLine($"\nMédia das notas : {mediaNotas}");
+    Console.WriteLine($"Total de alunos : {alunos.Count()}");
+}
