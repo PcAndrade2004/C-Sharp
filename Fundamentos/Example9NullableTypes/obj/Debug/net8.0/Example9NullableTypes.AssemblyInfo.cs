f28e59bac6ceba3612867bb123df06e0e5e40d6e
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-25 13:24:41 -0300

    Nullable Types

--- Fundamentos/Example9NullableTypes/obj/Debug/net8.0/Example9NullableTypes.AssemblyInfo.cs
+++ Fundamentos/Example9NullableTypes/obj/Debug/net8.0/Example9NullableTypes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example9NullableTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce12bf2306e3ed550c5d1d54ea33f302a8515c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example9NullableTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example9NullableTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
