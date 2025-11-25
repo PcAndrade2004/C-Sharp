e403ee31069f573d341ee97ecb11b8e38428cee9
Paulo Cesar Oliveira Andrade <pcandradeoliveira@gmail.com>
2025-11-25 19:03:58 -0300

    Convert

---
+++ Fundamentos/Exemple13Convert/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+Console.WriteLine("## Convert ##\n");
+
+// Convertendo string para int
+string numeroString = "100";
+int numeroConvertido = Convert.ToInt32(numeroString);
+Console.WriteLine($"Valor convertido para int: {numeroConvertido}");
+
+Console.WriteLine("------------------------------------------");
+
+// Convertendo string para decimal
+string valorDecimalString = "100.50";
+decimal valorDecimalConvertido = Convert.ToDecimal(valorDecimalString);
+Console.WriteLine($"Valor convertido para decimal: {valorDecimalConvertido.ToString("F2")}`);
+
+Console.ReadKey();
